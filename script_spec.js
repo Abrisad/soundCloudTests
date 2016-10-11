72b3ba0c036e2b07f03c455f2a4ff64fac35bbd5
Alexander Khotemskoy <Xotabu4@gmail.com>
2016-10-11 18:55:38 +0300

    test xit

--- script_spec.js
+++ script_spec.js
@@ -1,15 +1,34 @@
 "use strict";
 
-describe(`Home page`, function () {
+
+describe(`Home page`, function (){
+    
     
     it('should have correct title', function () {
         browser.get('https://soundcloud-ngrx.herokuapp.com/');
 
         expect(browser.getTitle()).toBe('SoundCloud • Angular2 NgRx', 
                   'Expecting title to be correct');
-    });
 
-    xit('should have correct URL', function () {
+        //let title = element(by.css('.content-header__title'));
+        // equals
+        let title = $('.content-header__title');
+
+        expect(title.getText()).toBe('Featured Tracks',
+         'Title on home page should be for Featured tracks');
+
+        //let link = element.all(by.css('.track-card__username')).first();
+        //const firstSong = $$('.track-card__username').first();
+        const firstSong = $$('.track-card__username').first();
+        let firstSongText = firstSong.getText();
+        firstSong.click();
+
+        const card_title = $('.user-card__title').getText();
+
+
+        expect(firstSongText).toBe(card_title);
+
+        firstSong.$('.nextelement').takeScreenshot();
         
     });
 
